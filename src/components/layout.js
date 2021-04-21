bef438f9cd0aa7548877982e710a50174aa484c5
eslamheshamm <esl2mhesham@gmail.com>
2021-04-21 20:12:53 +0200

    layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,55 +1,47 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
+import Logo from "./assets/logo"
+import Instagram from "./assets/instagram"
+import Youtube from "./assets/youtube"
+import Dribble from "./assets/dribble"
+import Twitter from "./assets/twitter"
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      <Header />
+
+      <main className=" font-manrope min-h-screen">{children}</main>
+      <footer className="flex flex-col md:flex-row justify-between items-center w-11/12 lg:w-10/12 mx-auto mb-6 ">
+        <p className="text-sm hidden lg:block">
+          © 2021 Owls Social. All rights reserved
+        </p>
+        <Logo className="order-2 md:order-1 mt-4 md:mt-0" />
+        <ul className="flex order-1 md:order-2">
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Instagram className="w-10 h-10 mr-4" />
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Youtube className="w-10 h-10 mr-4" />
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Dribble className="w-10 h-10 mr-4" />
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              <Twitter className="w-10 h-10 " />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
